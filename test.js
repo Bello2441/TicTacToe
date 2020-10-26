fc19aae078c3c97f279fb52f62eed78fd8f65e62
Rosenberg Bello <rosenbergbello@Rosenbergs-MacBook-Pro.local>
2020-10-26 19:25:01 -0400

    Still in development

--- test.js
+++ test.js
@@ -30,6 +30,13 @@ function Squares(props) {
     }
   
     render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + this.state.XisNext ? 'X' : 'O';
+      }
       const status = 'Next player: ' + this.state.XisNext ? 'X' : 'O';
   
       return (
@@ -70,7 +77,28 @@ function Squares(props) {
       );
     }
   }
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,4,8],
+      [1,4,7],
+      [2,4,6],
+      [0,3,6],
+      [2,5,8],
+    ];
+    for(let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+
+    }
+    return null;
+  }
+   
   // ========================================
   
   ReactDOM.render(
