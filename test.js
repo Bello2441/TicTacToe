66ec084c815201066c25e4b338fd5511c71d2353
Rosenberg Bello <rosenbergbello@Rosenbergs-MacBook-Pro.local>
2020-10-26 19:27:50 -0400

    Starting to finish

--- test.js
+++ test.js
@@ -18,6 +18,9 @@ function Squares(props) {
       }
       handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.XisNext ? 'X' : 'O';
         this.setState({
             squares:squares,
